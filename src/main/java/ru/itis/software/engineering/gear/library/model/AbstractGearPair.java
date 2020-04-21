1bb620e9600adb11d65c4b674296ef42d4dc6c07
ilmar <ilmar>
2020-04-21 17:21:36 +0300

    done

--- src/main/java/ru/itis/software/engineering/gear/library/model/AbstractGearPair.java
+++ src/main/java/ru/itis/software/engineering/gear/library/model/AbstractGearPair.java
@@ -1,7 +1,13 @@
 package ru.itis.software.engineering.gear.library.model;
 
 import ru.itis.software.engineering.gear.library.GearLibrary;
+import ru.itis.software.engineering.gear.library.Module;
 
+import javax.swing.plaf.PanelUI;
+
+/**
+ * абстрактная пара шестерен
+ */
 public abstract class AbstractGearPair {
 
     /**
@@ -13,6 +19,14 @@ public abstract class AbstractGearPair {
      * расстояние между центральным
      */
     Float centerDistance;
+
+
+
+    public AbstractGearPair(AbstractGear abstractGear1, AbstractGear abstractGear2, Float centerDistance) {
+        this.abstractGear1 = abstractGear1;
+        this.abstractGear2 = abstractGear2;
+        this.centerDistance = centerDistance;
+    }
 
     public AbstractGear getAbstractGear1() {
         return abstractGear1;
